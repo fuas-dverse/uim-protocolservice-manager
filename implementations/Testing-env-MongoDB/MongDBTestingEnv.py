7b0f57d9dfff44ba7c132348994a8c6ca04f339b
Rik Heerholtz <Rik.Heerholtz@gmail.com>
2025-10-08 11:20:29 +0200

    Mongo test cases find and add

---
+++ implementations/Testing-env-MongoDB/MongDBTestingEnv.py
@@ -0,0 +1,14 @@
+﻿from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["uim_database"]
+users = db["users"]
+
+print("All users")
+for u in users.find():
+    print(u)
+
+
+print("All handymen")
+for u in users.find({"role": "handymen"}):
+    print(u)
---
+++ implementations/Testing-env-MongoDB/MongoDBAdd.py
@@ -0,0 +1,7 @@
+﻿from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["uim_database"]
+users = db["users"]
+
+users.insert_many([{"name": "Mike", "role": "Developer"},{"name": "John", "role": "Handyman"}])
